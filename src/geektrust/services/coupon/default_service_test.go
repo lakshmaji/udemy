eb065ddee9ea2d18db804bd2eb32129e0cb1b7e8
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-09-25 13:22:23 +0530

    rename

--- src/geektrust/services/coupon/default_service_test.go
+++ src/geektrust/services/coupon/default_service_test.go
@@ -6,21 +6,22 @@ import (
 	"testing"
 )
 
+type testInput struct {
+	noOfPrograms int
+	subTotal     float64
+	coupons      []coupon.Coupon
+}
+
 func TestApplicableCoupon_WhenNoCouponApplied(t *testing.T) {
-	type TestInput struct {
-		noOfPrograms int
-		subTotal     float64
-		coupons      []coupon.Coupon
-	}
 
 	tt := []struct {
 		description string
-		input       TestInput
+		input       testInput
 		expected    coupon.Coupon
 	}{
 		{
 			description: fmt.Sprintf("when total no of programs are equal to %d, it should auto apply %s coupon", coupon.CouponB4G1MarginCount, coupon.CouponB4G1),
-			input: TestInput{
+			input: testInput{
 				noOfPrograms: 4,
 				subTotal:     3000,
 			},
@@ -28,7 +29,7 @@ func TestApplicableCoupon_WhenNoCouponApplied(t *testing.T) {
 		},
 		{
 			description: fmt.Sprintf("when total no of programs are more than %d, it should auto apply %s coupon", coupon.CouponB4G1MarginCount, coupon.CouponB4G1),
-			input: TestInput{
+			input: testInput{
 				noOfPrograms: 6,
 				subTotal:     19000,
 			},
@@ -36,7 +37,7 @@ func TestApplicableCoupon_WhenNoCouponApplied(t *testing.T) {
 		},
 		{
 			description: fmt.Sprintf("when total no of programs are less than %d, it should not apply %s coupon", coupon.CouponB4G1MarginCount, coupon.CouponB4G1),
-			input: TestInput{
+			input: testInput{
 				noOfPrograms: 2,
 				subTotal:     5000,
 			},
