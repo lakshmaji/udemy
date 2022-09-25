bbb40194f9ff6f7d26b37f11cc7e779e9da92417
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-09-25 13:17:23 +0530

    B4G1 should auto applied

---
+++ src/geektrust/services/coupon/default_service_test.go
@@ -0,0 +1,42 @@
+package coupon
+
+import (
+	"fmt"
+	"geektrust/core/coupon"
+	"testing"
+)
+
+func TestApplicableCoupon(t *testing.T) {
+	type TestInput struct {
+		noOfPrograms int
+		subTotal     float64
+		coupons      []coupon.Coupon
+	}
+
+	tt := []struct {
+		description string
+		input       TestInput
+		expected    coupon.Coupon
+	}{
+		{
+			description: fmt.Sprintf("when total no of programs are equal to %d, it should auto apply %s", coupon.CouponB4G1MarginCount, coupon.CouponB4G1),
+			input: TestInput{
+				noOfPrograms: 4,
+				subTotal:     3000,
+			},
+			expected: coupon.CouponB4G1,
+		},
+	}
+
+	for _, test := range tt {
+		t.Run(test.description, func(t *testing.T) {
+
+			printer := New()
+			received := printer.ApplicableCoupon(test.input.noOfPrograms, test.input.subTotal, test.input.coupons)
+
+			if received != test.expected {
+				t.Errorf("Expected %v, Received %v", test.expected, received)
+			}
+		})
+	}
+}
