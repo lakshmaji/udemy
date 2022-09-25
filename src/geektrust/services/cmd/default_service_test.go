39f635087dba9a69de8e0f3ea223858aead323b3
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-09-25 15:46:18 +0530

    command parser test cases improvements

--- src/geektrust/services/cmd/default_service_test.go
+++ src/geektrust/services/cmd/default_service_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	reader_client "geektrust/clients/reader"
+	"geektrust/utils"
 	"reflect"
 	"testing"
 	"testing/fstest"
@@ -55,6 +56,64 @@ func TestCommands(t *testing.T) {
 
 			if !reflect.DeepEqual(received, test.expected) {
 				t.Errorf("Expected %v, Received %v", test.expected, received)
+			}
+		})
+	}
+}
+
+func TestCommandsError(t *testing.T) {
+
+	type fileContent struct {
+		Data []byte
+	}
+
+	tt := []struct {
+		description string
+		files       []string
+		fileContent fstest.MapFS
+		expected    error
+	}{
+		{
+			description: "no file name was provided",
+			files:       []string{"main.go"},
+			fileContent: fstest.MapFS{},
+			expected:    utils.ErrorNoFilePath,
+		},
+		{
+			description: "no file content was provided",
+			files:       []string{"main.go", "input.txt"},
+			fileContent: fstest.MapFS{
+				"ouput.txt": {Data: []byte("")},
+			},
+			expected: utils.ErrorFileOpen,
+		},
+	}
+
+	for _, test := range tt {
+		t.Run(test.description, func(t *testing.T) {
+			// mock os.Args
+			originalOsArgs := reader_client.OsArgs
+			defer func() { reader_client.OsArgs = originalOsArgs }()
+
+			mockArgs := test.files
+			reader_client.OsArgs = mockArgs
+
+			// mock fs
+			fs := test.fileContent
+			var reader reader_client.BaseReader = reader_client.New(fs)
+			commandParser := New(reader)
+			received, err := commandParser.Commands()
+
+			if err == nil {
+				t.Error("should return error, received", err)
+			}
+
+			if test.expected != err {
+				t.Errorf("Expected %v, Received %v", test.expected, received)
+			}
+
+			if len(received) != 0 {
+				t.Errorf("Should not return commands, Received %v", received)
 			}
 		})
 	}
